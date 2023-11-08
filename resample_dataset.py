fb67532ab80b996746e0d80ff9df2e623fdfbb01
MahmoudUwk <mahmouduwk338@gmail.com>
2023-11-08 15:58:17 -0500

    update

--- resample_dataset.py
+++ resample_dataset.py
@@ -17,37 +17,31 @@ sav_path = "C:/Users/msallam/Desktop/Kuljeet/resampled data"
 
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f)) and '.csv' in f]
 
+def resample(df,txt):
+    df_downsampled = df.resample(txt).mean()
+    del df
+    df_downsampled = df_downsampled.dropna()
+    df_downsampled.to_csv(os.path.join(sav_path,txt+'.csv'))
+    print(df_downsampled.shape)
 
 for counter , file in enumerate(onlyfiles):
     full_path = os.path.join(data_path,file)
     if counter == 0:
         df = pd.read_csv(full_path)
     else:
-        # df = df.append(pd.read_csv(full_path))
-        
         df = pd.concat([df, pd.read_csv(full_path)]).sort_values('timestamp').reset_index(drop=True)
 print(df.shape)
 #%%
 df.set_index(pd.to_datetime(df.timestamp), inplace=True)
-df = df.dropna()
-df.to_csv(os.path.join(sav_path,'1Hz.csv'))
+# df = df.dropna()
+# df.to_csv(os.path.join(sav_path,'1Hz.csv'))
 # df = pd.read_csv(data_path)
 
-df_downsampled = df.resample('1T').mean()
-df_downsampled = df_downsampled.dropna()
-df_downsampled.to_csv(os.path.join(sav_path,'1T.csv'))
+resample(df,'1T')
 
-df_downsampled10 = df.resample('10T').mean()
-df_downsampled10 = df_downsampled10.dropna()
-df_downsampled10.to_csv(os.path.join(sav_path,'10T.csv'))
+resample(df,'10T')
 
-df_downsampled15 = df.resample('15T').mean()
-df_downsampled15 = df_downsampled15.dropna()
-df_downsampled15.to_csv(os.path.join(sav_path,'15T.csv'))
+resample(df,'15T')
 
+resample(df,'30T')
 
-df_downsampled30 = df.resample('30T').mean()
-df_downsampled30 = df_downsampled30.dropna()
-df_downsampled30.to_csv(os.path.join(sav_path,'30T.csv'))
-# df.drop(columns=["timestamp"], inplace=True)
-# df = df['P']
